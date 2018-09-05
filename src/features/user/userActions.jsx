d36a6c052960e37d769a43ec56ec8ed3afdea50a
Developer <minhkha791140@gmail.com>
2018-09-05 08:01:48 -0700

    End of section 16

---
+++ src/features/user/userActions.jsx
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import { toastr } from 'react-redux-toastr';
+
+export const updateProfile = user => async (dispatch, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+  const { isLoaded, isEmpty, ...updateUser } = user;
+  if (updateUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
+    updateUser.dateOfBirth = moment(updateUser.dateOfBirth).toDate();
+  }
+  try {
+    await firebase.updateProfile(updateUser);
+    toastr.success('Success', 'Profile Updated');
+  } catch (error) {
+    console.log(error);
+  }
+};
